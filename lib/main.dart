7cdc20d8150e58f2ac84efa37343729fcf2898fd
Kurogoma939 <kan.sss.939@gmail.com>
2023-01-13 20:20:22 +0900

    サンプルTODOのページ整理

--- lib/main.dart
+++ lib/main.dart
@@ -49,94 +49,16 @@ class _MyHomePageState extends State<MyHomePage> {
           child: SingleChildScrollView(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                FetchDataButton(),
-                InsertDataButton(),
-                UpdateDataButton(),
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('サンプルTODO'),
+                )
               ],
             ),
           ),
         ),
       ),
-    );
-  }
-}
-
-class FetchDataButton extends StatefulWidget {
-  const FetchDataButton({super.key});
-
-  @override
-  State<FetchDataButton> createState() => _FetchDataButtonState();
-}
-
-class _FetchDataButtonState extends State<FetchDataButton> {
-  // SQL取得結果
-  String fetchResult = '';
-
-  @override
-  Widget build(BuildContext context) {
-    final supabase = Supabase.instance.client;
-    return Column(
-      children: [
-        ElevatedButton(
-          child: const Text('Fetch Data'),
-          onPressed: () async {
-            // supabaseからデータを取得
-            final response = await supabase
-                .from('todo')
-                .select()
-                .order('id', ascending: true);
-            setState(() {
-              fetchResult = response.toString();
-            });
-          },
-        ),
-        Text(fetchResult),
-      ],
-    );
-  }
-}
-
-/// 新規データ追加
-class InsertDataButton extends StatefulWidget {
-  const InsertDataButton({super.key});
-
-  @override
-  State<InsertDataButton> createState() => _InsertDataButtonState();
-}
-
-class _InsertDataButtonState extends State<InsertDataButton> {
-  @override
-  Widget build(BuildContext context) {
-    final supabase = Supabase.instance.client;
-    return ElevatedButton(
-      child: const Text('Insert Data'),
-      onPressed: () async {
-        // supabaseにデータを追加
-        final response = await supabase.from('todo').insert([
-          {'id': 2, 'name': 'test'},
-        ]);
-        print(response);
-      },
-    );
-  }
-}
-
-/// データ更新ボタン
-class UpdateDataButton extends StatelessWidget {
-  const UpdateDataButton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final supabase = Supabase.instance.client;
-    return ElevatedButton(
-      child: const Text('Update Data'),
-      onPressed: () async {
-        // supabaseのidが1のデータを更新
-        final response =
-            await supabase.from('todo').update({'name': 'テストデータ'}).eq('id', 1);
-        print(response);
-      },
     );
   }
 }
